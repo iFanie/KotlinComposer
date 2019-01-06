27ec2b7793441c3f054782c528570027c72eda22
fanisveizi <f.veizi@thebeat.co>
2019-01-07 00:14:54 +0200

    Run project-wise auto import optimization

--- app/src/main/java/com/izikode/izilib/kotlincomposer/app/MainActivity.kt
+++ app/src/main/java/com/izikode/izilib/kotlincomposer/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.izikode.izilib.kotlincomposer.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.izikode.izilib.kotlincomposer.dsl.*
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
