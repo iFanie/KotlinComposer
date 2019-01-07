61f6197e9ddf75d1db725f2f74466526364b7ee8
fanisveizi <f.veizi@thebeat.co>
2019-01-07 18:11:42 +0200

    Update sample with new simpleClass and generation code

--- app/src/main/java/com/izikode/izilib/kotlincomposer/app/MainActivity.kt
+++ app/src/main/java/com/izikode/izilib/kotlincomposer/app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import com.izikode.izilib.kotlincomposer.dsl.*
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.TypeSpec
 
 class MainActivity : AppCompatActivity() {
@@ -17,46 +18,68 @@ class MainActivity : AppCompatActivity() {
         print(withComposer)
     }
 
-    val withoutComposer = (FileSpec.builder("com.izikode.izilib.kotlincomposer.app", "SimpleClass")
-        .addType(
-            TypeSpec.classBuilder("SimpleClass")
-                .addFunction(
-                    FunSpec.builder("helloWorld")
+    val withoutComposer = (
+
+            FileSpec.builder("com.izikode.izilib.kotlincomposer.app", "SimpleClass")
+                .addType(
+                    TypeSpec.classBuilder("SimpleClass")
+                        .addFunction(
+                            FunSpec.builder("helloWorld")
+                                .addCode(
+                                    """
+                                        |print("Hello world!")
+                                    """.trimMargin()
+                                ).build()
+                        ).addFunction(
+                            FunSpec.builder("privateHelloWorld")
+                                .addModifiers(KModifier.PRIVATE)
+                                .addCode(
+                                    """
+                                        |print("Hello world, in private.")
+                                    """.trimMargin()
+                                ).build()
+                        ).build()
+                ).addFunction(
+                    FunSpec.builder("topHelloWorld")
                         .addCode(
                             """
-                                |print("Hello world!")
+                                |print("Hello world from top level!")
                             """.trimMargin()
                         ).build()
                 ).build()
-        ).addFunction(
-            FunSpec.builder("topHelloWorld")
-                .addCode(
-                    """
-                        |print("Hello world from top level!")
-                    """.trimMargin()
-                ).build()
-        ).build()).toString()
 
-    val withComposer = (compose a file("SimpleClass") {
-        compose a `package`("com.izikode.izilib.kotlincomposer.app") {
+        ).toString()
 
-            compose a `class`("SimpleClass") {
+    val withComposer = (
 
-                compose a `fun`("helloWorld") {
-                    """
-                        |print("Hello world!")
-                    """.trimMargin()
+            compose a file("SimpleClass") {
+                compose a `package`("com.izikode.izilib.kotlincomposer.app") {
+
+                    compose a `class`("SimpleClass") {
+
+                        compose a `fun`("helloWorld") {
+                            """
+                                |print("Hello world!")
+                            """.trimMargin()
+                        }
+
+                        compose a `fun`("privateHelloWorld") {
+                            """
+                                |print("Hello world, in private.")
+                            """.trimMargin()
+                        } `as` private
+
+                    }
+
+                    compose a `fun`("topHelloWorld") {
+                        """
+                            |print("Hello world from top level!")
+                        """.trimMargin()
+                    }
+
                 }
-
             }
 
-            compose a `fun`("topHelloWorld") {
-                """
-                    |print("Hello world from top level!")
-                """.trimMargin()
-            }
-
-        }
-    }).toString()
+        ).toString()
 
 }
